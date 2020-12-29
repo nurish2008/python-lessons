003fd68b7c81f0fd0022e9113489071f7f97e4d3
Nuriddin <76534748+nurish2008@users.noreply.github.com>
2020-12-29 20:13:15 +0500

    New lesson added

---
+++ Lesson-14.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 25 00:07:35 2020
+
+@author: Sony
+"""
+
+# car_0 = {'model':'ferrari', 'rangi':'qizil'}
+# print(car_0['model'])
+# print(car_0['rangi'])
+
+# talaba_0 = {'ism':'murod olimov', 'yosh':20, 't_yil':2000}
+# print(f"{talaba_0['ism'].title()},\
+#       {talaba_0['t_yil']}- yilda tugilgan,\
+#       {talaba_0['yosh']} yoshda")
+      
+# talaba_0['kurs'] = 4
+# talaba_0['fakultet'] = 'informatika'
+# print(talaba_0)
+
+# talaba_1 = {} 
+
+# talaba_1['ism'] = 'Qobil Rasulov'
+# talaba_1['kurs'] = 3
+# talaba_1['yosh'] = 20
+# print(talaba_1)
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+
+# talaba_1['kurs'] = 4 
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+
+
+
+# talaba_0 = {'ism':'murod olimov','yosh':20,'t_yil':2000}
+# print(talaba_0)
+# del talaba_0['yosh'] # yosh degan kalit so'z (va qiymatni) o'chiramiz
+# print(talaba_0)
+
+# telefonlar = {
+#     'ali':'iphone X', 
+#     'vali':'galaxy S9',
+#     'olim':'mi 10 pro',
+#     'orif':'nokia3310'
+#     }
+# phone = telefonlar['ali']
+# print(f'Alining telefoni {phone}')
+
+# phone = telefonlar.get('hasan', 'Bunday ism mavjud emas')
+# print(phone)
+
+# phone = telefonlar.get('hasan')
+# print(phone)
+
+
+    
+    
---
+++ Lesson-14homework.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 25 20:01:44 2020
+
+@author: Sony
+"""
+
+# otam = {'ism':' fahritdin', 't_yil':1953, 'adres':'toshkent-olmazor tuman quyosh 62uyda tugilgan',} 
+# t_yil = otam['t_yil']
+# adres = otam['adres']
+# print(f"Otamning ismi{otam['ism'].title()}, {t_yil}-yilda, {adres}")
+
+# onam = {'ism':' muattar', 't_yil':'1955-yilda', 'adres':'Toshkent-olmazor tuman quyosh m.golib 23uyda tugilgan',}
+# t_yil1 = onam['t_yil']
+# adres1 = onam['adres']
+# print(f"Onamning ismi{onam['ism'].title()}, {t_yil1}-yilda, {adres1}")
+
+
+# akam = {'ism':' husniddin', 't_yil':'1979-yilda', 
+#           'adres':'toshkent-olmazor tuman quyosh 62uyda tugilgan',} 
+# t_yil2 = akam['t_yil']
+# adres2 = akam['adres']
+# print(f"Akamning ismi{akam['ism'].title()}, {t_yil2}-yilda, {adres2}")
+
+# singlim = {'ism':' nodira', 't_yil':'1984-yilda', 
+#         'adres':'toshkent-olmazor tuman quyosh 62uyda tugilgan',}
+# t_yil3 = singlim['t_yil']
+# adres3 = singlim['adres']
+# print(f"Singlimning ismi{singlim['ism'].title()}, {t_yil3}-yilda, {adres3}")
+
+
+# taomlar = {
+#     'sayfiddin':'lagmon', #'gamburger','lavash',
+#     'nazokat':'chechevitsa',#'noxotshorva','osh',
+#     'sora':'moshxurdi', #'osh','xlopya'
+#     'nuriddin':'osh',#'manti'
+#     }
+# ovqat = taomlar['sayfiddin']
+# print(f"Sayfidding sevimli taomlari {ovqat}")
+# ovqat = taomlar['nazokat']
+# print(f"Nazokatning sevimli taomlari {ovqat}")
+# ovqat = taomlar['sora']
+# print(f"soraning sevimli taomlari {ovqat}")
+# ovqat = taomlar['nuriddin']
+# print(f"nuriddining sevimli taomlari {ovqat}")
+
+# python = {
+#     'integer':'son kiritish komandasi',
+#     'float':'Butun sonni kursatish komandasi',
+#     'string':'Matn yozish komandasi',
+#     'if':'yoki 2ta yol tanlash komandasi'
+#     }
+# komanda = python['integer']
+# print(f"Pythonda integer {komanda} deb ataladi")
+# komanda = python['float']
+# print(f"Pythonda float {komanda}deb ataladi")
+# komanda = python['string']
+# print(f"Pythonda string {komanda}deb ataladi")
+# komanda=python['if']
+# print(f"Pythonda if {komanda} deb ataladi")
+
+# python_izohli_lugati = {
+#     'integer':"Butun son",
+#     'float':"O'nlik son",
+#     'string':"Matn",
+#     'list':"Ro'yxat",
+#     'tuple':"O'zgarmas ro'yxat"
+#     }
+# # print(f"python_izohli_lugati['tuple']")
+
+# kalit = input('Kalit soz kiriting?...').lower()
+# print(python_izohli_lugati.get(kalit,"Bunday soz mavjud emas"))
+
+
+# python_izohli_lugati = {
+#     'integer':"Butun son",
+#     'float':"O'nlik son",
+#     'string':"Matn",
+#     'list':"Ro'yxat",
+#     'tuple':"O'zgarmas ro'yxat"
+#     }
+# kalit = input('Kalit soz kiriting?...').lower()
+# tarjima = python_izohli_lugati.get(kalit) 
+# if tarjima == None:
+#     print('Bunday soz mavjud emas')    
+# else:
+#     print(f'{kalit.title()} sozi {tarjima} deb tarjima qilinadi')
+    
