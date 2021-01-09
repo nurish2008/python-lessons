5c6fddad2a7c6fa7594a658271bccf19d27082b3
Nuriddin <76534748+nurish2008@users.noreply.github.com>
2021-01-09 17:59:51 +0500

    Create Lesson-18homework.py

---
+++ Lesson-18homework.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  8 16:42:54 2021
+
+@author: Lesson 18 homework
+"""
+
+# 1 - Assignment
+# print('Mahsulotlar nomini kiritamiz!!')
+# savat =[]
+# while True:
+#     savol = input("Savatga mahsulotlarni qushing: ")
+#     savat.append(savol)
+#     javob = input('Yana mahsulot qushasizmi? (ha/yoq)')
+#     if javob !='ha':
+#         break
+# print("Mahsulotlaringiz ruyhati quydagilar")
+
+# 2- Assignment
+
+# mahsulotlar = {}
+# ishora = True
+# while ishora: 
+#     ism = input("Mahsulot nomini kiriting: ")
+#     mahsulot = input(f"{ism.title()}ning narhini kiriting: ")
+#     mahsulotlar[ism] = int(mahsulot)
+    
+#     javob = input("Yana mahsulot qushasizmi? (ha/yoq)")
+#     if javob == 'yoq':
+#         ishora = False
+
+# 3- Assignment
+
+# buyurtmalar = ['olma', 'anjir', 'uzum', 'qovun']
+# mahsulotlar = {'olma': 4000,
+#                'anjir':10000,
+#                'uzum':15000,
+#                'qovun':12000,
+#                'shaftoli':25000,
+#                'tarvuz':10000}
+# while buyurtmalar:
+#     buyurtma = buyurtmalar.pop()
+#     if buyurtma in mahsulotlar.keys():
+#         narh = mahsulotlar[buyurtma]
+        
+#         print(f"{buyurtma.title()} -{narh} som ")
+#     else:
+#         print(f"Bizda {buyurtma} yoq kechirasiz ")
+        
+
+            
